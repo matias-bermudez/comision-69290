bee19d39f548bdeb26bf9dc339d31ea8d19740c8
matias bermudez <matias.bermudez.munoz@gmail.com>
2025-05-03 22:31:12 -0300

    falta implementar local storage y mostrar dinamicamente ganador y puntuacion.

--- script/index.js
+++ script/index.js
@@ -1,16 +1,14 @@
 const jugador1 = {
     nick: '',
-    puntos: 0,
-    cartas: ["vacio", "vacio", "vacio"],
-    mano: true,
+    puntosMano: 0,
+    cartas: [],
     puntosPartido: 0,
 };
 
 const jugador2 = {
     nick: '',
     puntosMano: 0,
-    cartas: ["vacio", "vacio", "vacio"],
-    mano: false,
+    cartas: [],
     puntosPartido: 0,
 };
 
@@ -33,7 +31,9 @@ class Carta {
     }
 }
 
-const muestra = new Carta(-1, -1);
+const Mesa = {
+    jugadas: ["", ""],
+}
 
 //La asignacion esta justificada por la cantidad de espacios que tiene cada carta en sus contorno.
 const traduccionPalo = (palo) => {
@@ -86,32 +86,6 @@ let valores = [
             [8, 9, 10, 1, 2, 3, 4, 5, 6, 7]
             ];
 
-const valoresConMuestra = () => {
-    const palo = traduccionPaloInverso(muestra.getPalo());
-    const numero = muestra.getNumero();
-    valores[palo][traduccionNumero(2)] = 19;
-    valores[palo][traduccionNumero(4)] = 18;
-    valores[palo][traduccionNumero(5)] = 17;
-    valores[palo][traduccionNumero(11)] = 16;
-    valores[palo][traduccionNumero(10)] = 15;
-    if ((numero == 2) || (numero == 4) || (numero == 5) || (numero == 10) || (numero == 11)) {
-        switch (palo) {
-            case 0:
-                valores[0][traduccionNumero(12)] = valores[0][traduccionNumero(numero)];
-                break;
-            case 1:
-                valores[1][traduccionNumero(12)] = valores[1][traduccionNumero(numero)];
-                break;
-            case 2:
-                valores[2][traduccionNumero(12)] = valores[2][traduccionNumero(numero)];
-                break;
-            case 3:
-                valores[3][traduccionNumero(12)] = valores[3][traduccionNumero(numero)];
-                break;
-        }
-    }
-}
-
 const reiniciarValores = () => {
     valores.length = 0;
     valores.push([8, 9, 10, 1, 2, 3, 4, 5, 6, 7]); 
@@ -138,10 +112,6 @@ const reiniciarMazo = () => {
 const reiniciarMano = (jugador1, jugador2) => {
     jugador1.cartas.length = 0;
     jugador2.cartas.length = 0;
-    for (let i = 1; i <= 3; i++) {
-        jugador1.cartas.push("");
-        jugador2.cartas.push("");
-    }
 }
 
 //Retorna valor del 0 al 3. (4 posibles palos) 
@@ -171,53 +141,12 @@ const getCombinacionValida = () => {
 }
 
 // Reparte una carta a un jugador.
-const repartirCartaJ1 = (jugador) => {
-    carta = getCombinacionValida();
+const repartirCarta = (jugador) => {
+    const carta = getCombinacionValida();
     jugador.cartas.push(carta);
-    jugador.cartas.shift();
-    const contenedor = document.createElement("div");
-    const numero = document.createElement("h3");
-    const palo = document.createElement("h3");
-
-    contenedor.classList.add("carta");
-    numero.classList.add("numero");
-    palo.classList.add("palo");
-
-    numero.textContent = carta.getNumero();
-    palo.textContent = carta.getPalo();
-
-    contenedor.appendChild(palo);
-    contenedor.appendChild(numero)
-
-    const destino = document.querySelector("body .mesa-juego .cartas .jugador1");
-    destino.appendChild(contenedor);
-}
-
-const repartirCartaJ2 = (jugador) => {
-    carta = getCombinacionValida();
-    jugador.cartas.push(carta);
-    jugador.cartas.shift();
-
-    const contenedor = document.createElement("div");
-    const numero = document.createElement("h3");
-    const palo = document.createElement("h3");
-
-    contenedor.classList.add("carta");
-    numero.classList.add("numero");
-    palo.classList.add("palo");
-
-    numero.textContent = carta.getNumero();
-    palo.textContent = carta.getPalo();
-
-    contenedor.appendChild(palo);
-    contenedor.appendChild(numero)
-
-    const destino = document.querySelector("body .mesa-juego .cartas .jugador2");
-    destino.appendChild(contenedor);
 }
 
 const irAlMazo = (jugador1, jugador2) => {
-    console.log(jugador1.nick+ ": Me voy al mazo");
     reiniciarMano(jugador1, jugador2);
     reiniciarMazo();
 }
@@ -226,140 +155,196 @@ const stringCarta = (carta) => {
     return carta.numero + " de " + carta.palo;
 }
 
-// Reparte 1 carta a cada jugador hasta tener 3 cartas c/u. Retorna la muestra.
+// Reparte 1 carta a cada jugador hasta tener 3 cartas c/u.
 const barajarRepartir = (jugador1, jugador2) => {
-    if (jugador1.mano) {
-        for (let i = 1; i <= 6; i++ ) {
-            repartirCartaJ2(jugador2);
-            repartirCartaJ1(jugador1);
-        }
-    } else {
-        for (let i = 1; i <= 6; i++ ) {
-            repartirCartaJ1(jugador1);
-            repartirCartaJ2(jugador2);
-        }
+    for (let i = 1; i <= 3; i++ ) {
+        repartirCarta(jugador2);
+        repartirCarta(jugador1);
     }
-    aux = getCombinacionValida();
-    muestra.palo = aux.getPalo();
-    muestra.numero = aux.getNumero();
     return 0;
 }
 
-//Funcion que selecciona la carta a jugar por jugador.
-const jugarCarta = (jugador) => {
-    console.log("Juega " + jugador.nick);
-    console.log("Selecciona 0: Me voy al mazo");
-    let contador = 1;
-    for (let carta of jugador.cartas) {
-        console.log("Selecciona " + contador + ": Jugar " + stringCarta(carta));
-        contador++
-    }
-    let opcion = parseInt(prompt("Seleccione jugada."));
-    while (opcion < 0 || opcion > jugador.cartas.length || isNaN(opcion)) {
-        opcion = parseInt(prompt("Seleccione jugada valida"));
-    }
-    console.log("");
-    return opcion;
-}
-
-const compararValores = (jugador1, jugador2, cartaj1, cartaj2, valores) => {
-    if(valores[traduccionPaloInverso(cartaj1.palo)][traduccionNumero(cartaj1.numero)] <
+const compararValores = (jugador1, jugador2, cartaj1, cartaj2) => {
+    if(valores[traduccionPaloInverso(cartaj1.palo)][traduccionNumero(cartaj1.numero)] <=
         valores[traduccionPaloInverso(cartaj2.palo)][traduccionNumero(cartaj2.numero)] ) {
-            jugador2.puntos++;
-            return 2;
+            jugador2.puntosMano++;
+            return 0;
         } else {
-            jugador1.puntos++;
-            return 1;
+            jugador1.puntosMano++;
+            return 0;
         }
 }
 
-const jugarManoAMano = (jugador1, jugador2, fn) => {
-    let jugadaJ1 = 1;
-    let jugadaJ2 = 1;
-    jugadaJ2 = jugarCarta(jugador2);
-    if (jugadaJ2 === 0) {
-        jugador1.puntosPartido++;
-        return 0;
-    } else {
-        console.log("La carta jugada por " + jugador2.nick + " es " + stringCarta(jugador2.cartas[jugadaJ2 - 1]));
-        console.log("");
-        const CartaJ2 = new Carta((jugador2.cartas[jugadaJ2 - 1].palo), (jugador2.cartas[jugadaJ2 - 1].numero));
-        jugador2.cartas.splice(jugadaJ2 - 1, 1);
-        jugadaJ1 = jugarCarta(jugador1);
-        if (jugadaJ1 === 0) {
-            jugador2.puntosPartido++;
-            irAlMazo(jugador1, jugador2);
+const noJugoJ2 = () => {
+    const paloj2 = document.getElementById('paloj2').innerText;
+    const numeroj2 = document.getElementById('numeroj2').innerText;
+    if(paloj2 === "" || numeroj2 ==="") {
+        return true;
+    } else return false;
+}
+
+const noJugoJ1 = () => {
+    const paloj1 = document.getElementById('paloj1').innerText;
+    const numeroj1 = document.getElementById('numeroj1').innerText;
+    if(paloj1 === "" || numeroj1 === "") {
+        return true;
+    } else return false;
+}
+
+const vaciarCartas = () => {
+    const contenedor1 = document.querySelector('.mesa-juego .cartas .jugadores .jugador1');
+    contenedor1.innerHTML= ``;
+    const contenedor2 = document.querySelector('.mesa-juego .cartas .jugadores .jugador2');
+    contenedor2.innerHTML= ``;
+}
+
+const actualizarPuntuacion = (jugador1, jugador2) => {
+    const puntos = document.createElement("h3");
+    const destino = document.querySelector(".mesa-juego");
+    puntos.innerText=`${jugador1.puntosPartido} - ${jugador2.puntosPartido}`;
+    destino.appendChild(puntos);
+} 
+
+const vaciarCartasJugadas = () => {
+    const paloj1 = document.getElementById('paloj1');
+    paloj1.textContent = "";
+    const numeroj1 = document.getElementById('numeroj1');
+    numeroj1.textContent = "";
+
+    const paloj2 = document.getElementById('paloj2');
+    paloj2.textContent = "";
+    const numeroj2 = document.getElementById('numeroj2');
+    numeroj2.textContent = "";
+}
+
+const eliminarCarta = (jugador, carta, fn) => {
+    let contador = 0;
+    let id = 0;
+    jugador.cartas.forEach(cartaAct => {
+        if(cartaAct.getPalo() == carta.getPalo() && cartaAct.getNumero() == carta.getNumero()) {
+            id = contador;
         } else {
-            console.log("La carta jugada por " + jugador1.nick + " es " + stringCarta(jugador1.cartas[jugadaJ1 - 1]));
-            console.log("");
-            const CartaJ1 = new Carta((jugador1.cartas[jugadaJ1 - 1].palo), (jugador1.cartas[jugadaJ1 - 1].numero));
-            jugador1.cartas.splice(jugadaJ1 - 1, 1);
-            return fn(jugador1, jugador2, CartaJ1, CartaJ2, valores);
+            contador++;
         }
+    });
+    jugador.cartas.splice(id, 1);
+    fn();
+}
+
+const jugadorGano = (jugador) => {
+    if(jugador.puntosPartido == 2) {
+        return true;
+    } else {
+        return false;
     }
 }
 
-//Jugamos la mano, actualmente solo muestra las cartas de cada jugador y da la opcion de jugar una carta
-// o irse al mazo terminando la mano. No esta implementado sistema de puntos.
-const jugarMano = (jugador1, jugador2) => {
-    let caso = -1;
-    let cantIteraciones = 1;
-    barajarRepartir(jugador1, jugador2);
-    valoresConMuestra();
-    console.log("La muestra es: " + muestra.getNumero() + " de " + muestra.getPalo());
-    if(jugador1.mano) {
-        while((cantIteraciones <= 3) && !(caso === 0)) {
-            caso = jugarManoAMano(jugador1, jugador2, compararValores);
-            if(jugador1.puntos === 2) {
-                jugador1.puntosPartido++;
-                console.log("j1 gano mano");
-                break;
-            } else if(jugador2.puntos === 2) {
-                jugador2.puntosPartido++;
-                console.log("j2 gano mano");
-                break;
+const imprimirCartas = (jugador1, jugador2) => {
+    //actualizarPuntuacion(jugador1, jugador2);
+    jugador1.cartas.forEach(carta => {
+        const card = document.createElement("div");
+        card.classList.add("carta");
+        card.innerHTML=`
+            <h3 class="palo">${carta.getPalo()}</h3>
+            <h3 class="numero">${carta.getNumero()}</h3>
+            <button id="${carta.getPalo()}${carta.getNumero()}">Jugar</button>
+        `
+        const destino = document.querySelector("body .mesa-juego .cartas .jugadores .jugador1");
+        destino.appendChild(card);
+        const boton = document.getElementById(`${carta.getPalo()}${carta.getNumero()}`);
+        boton.addEventListener("click", () => {
+            let palo = document.getElementById('paloj1');
+            let numero = document.getElementById('numeroj1');
+            if(palo.innerText !== "" && numero.innerText !== "") {
+                /*const alerta = document.createElement("h3");
+                alerta.classList.add("alerta");
+                alerta.innerText = jugador1.nick + " ya jugó, compare cartas.";
+                const destinoAlerta = document.querySelector(".mesa-juego .cartas .jugadores .jugador1 .alerta");
+                destinoAlerta.appendChild(alerta);*/
+            } else {
+                palo.innerText = carta.getPalo();
+                numero.innerText = carta.getNumero();
+                const CartaJugada = new Carta(palo.innerText, parseInt(numero.innerText));
+                eliminarCarta(jugador1, CartaJugada, vaciarCartas);
+                imprimirCartas(jugador1, jugador2);
+                let palo2 = document.getElementById('paloj2');
+                let numero2 = document.getElementById('numeroj2');
+                const cartaj2 = new Carta(palo2.innerText, parseInt(numero2.innerText));
+                if(!noJugoJ2()) {
+                    compararValores(jugador1, jugador2, CartaJugada, cartaj2);
+                    vaciarCartasJugadas();
+                }
             }
-            cantIteraciones++;
-        }
-        jugador1.mano = false;
-        jugador2.mano = true;
-    } else {
-        while((cantIteraciones <= 3) && !(caso === 0)) {
-            caso = jugarManoAMano(jugador2, jugador1, compararValores);
-            if(jugador2.puntos === 2) {
-                jugador2.puntosPartido++;
-                console.log("j2 gano mano");
-                break;
-            } else if(jugador1.puntos === 2) {
-                jugador1.puntosPartido++;
-                console.log("j1 gano mano");
-                break;
+        });
+    });
+
+    jugador2.cartas.forEach(carta => {
+        const card = document.createElement("div");
+        card.classList.add("carta");
+        card.innerHTML=`
+            <h3 class="palo">${carta.getPalo()}</h3>
+            <h3 class="numero">${carta.getNumero()}</h3>
+            <button id="${carta.getPalo()}${carta.getNumero()}">Jugar</button>
+        `
+        const destino = document.querySelector("body .mesa-juego .cartas .jugadores .jugador2");
+        destino.appendChild(card);
+        const boton = document.getElementById(`${carta.getPalo()}${carta.getNumero()}`);
+        boton.addEventListener("click", () => {
+            let palo = document.getElementById('paloj2');
+            let numero = document.getElementById('numeroj2');
+            if(palo.innerText !== "" && numero.innerText !== "") {
+                
+            } else {
+                palo.innerText = carta.getPalo();
+                numero.innerText = carta.getNumero();
+                const CartaJugada = new Carta(palo.innerText, parseInt(numero.innerText));
+                eliminarCarta(jugador2, CartaJugada, vaciarCartas);
+                imprimirCartas(jugador1, jugador2);
+                let palo1 = document.getElementById('paloj1');
+                let numero1 = document.getElementById('numeroj1');
+                const cartaj1 = new Carta(palo1.innerText, parseInt(numero1.innerText));
+                if(!noJugoJ1()) {
+                    compararValores(jugador1, jugador2, cartaj1, CartaJugada);
+                    vaciarCartasJugadas();
+                }
             }
-            cantIteraciones++;
-        }
-        jugador1.mano = true;
-        jugador2.mano = false;
+        });
+    });
+    if(jugador1.cartas.length === 0 && jugador2.cartas.length === 0) {
+        if(!jugadorGano(jugador1) && !jugadorGano(jugador2)) {
+            if(jugador1.puntosMano == 2) {
+                jugador1.puntosPartido++
+                jugador1.puntosMano = 0;
+                jugador2.puntosMano = 0;
+            } else if(jugador2.puntosMano == 2) {
+                jugador2.puntosPartido++
+                jugador1.puntosMano = 0;
+                jugador2.puntosMano = 0;
+            } 
+            if(!jugadorGano(jugador1) && !jugadorGano(jugador2)) {
+                barajarRepartir(jugador1, jugador2);
+                imprimirCartas(jugador1, jugador2);
+            }
+            } else if (jugadorGano(jugador1)) {
+            }   else {
+            } 
     }
-    irAlMazo(jugador1, jugador2);
-    reiniciarValores();
 }
 
 const jugarPartido = (jugador1, jugador2) => {
-    while(jugador1.puntosPartido < 2 && jugador2.puntosPartido < 2) {
-        jugarMano(jugador1, jugador2);
-    }
-    console.log("¡Buen partido!"); 
+    jugador1.puntosPartido = 0;
+    jugador2.puntosPartido = 0;
+    jugador1.puntosMano = 0;
+    jugador2.puntosMano = 0;
+    barajarRepartir(jugador1, jugador2);
+    actualizarPuntuacion(jugador1, jugador2);
+    imprimirCartas(jugador1, jugador2);
 }
 
-boton.addEventListener("click", function(){
+boton.addEventListener("click", function(event){
+    event.preventDefault();
     jugador1.nick = nickJ1.value;
     jugador2.nick = nickJ2.value;
     jugarPartido(jugador1, jugador2);
 })
-
-
-
-
-
-
 
