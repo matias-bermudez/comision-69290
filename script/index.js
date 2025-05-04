34930da167f21fb3645b29b7357052a93b29966f
matias bermudez <matias.bermudez.munoz@gmail.com>
2025-05-04 14:23:06 -0300

    Se completo la muestra dinamica de cartas y cartas jugadas, tambien la suma de puntos dinamico al final de cada mano, mostrando el ganador en caso de ganar 2 manos.

--- script/index.js
+++ script/index.js
@@ -80,19 +80,11 @@ const traduccionNumero = (numero) => {
 }
 
 let valores = [
-            [8, 9, 10, 1, 2, 3, 4, 5, 6, 7], 
+            [8, 9, 10, 1, 2, 3, 11, 5, 6, 7], 
             [8, 9, 10, 1, 2, 3, 4, 5, 6, 7],
-            [8, 9, 10, 1, 2, 3, 4, 5, 6, 7],
-            [8, 9, 10, 1, 2, 3, 4, 5, 6, 7]
+            [14, 9, 10, 1, 2, 3, 12, 5, 6, 7],
+            [13, 9, 10, 1, 2, 3, 4, 5, 6, 7]
             ];
-
-const reiniciarValores = () => {
-    valores.length = 0;
-    valores.push([8, 9, 10, 1, 2, 3, 4, 5, 6, 7]); 
-    valores.push([8, 9, 10, 1, 2, 3, 4, 5, 6, 7]);
-    valores.push([8, 9, 10, 1, 2, 3, 4, 5, 6, 7]);
-    valores.push([8, 9, 10, 1, 2, 3, 4, 5, 6, 7]);
-}
 
 let mazo = [
             [1, 2, 3, 4, 5, 6, 7, 10, 11, 12], 
@@ -112,6 +104,11 @@ const reiniciarMazo = () => {
 const reiniciarMano = (jugador1, jugador2) => {
     jugador1.cartas.length = 0;
     jugador2.cartas.length = 0;
+}
+
+const reiniciarPtosMano = (j1, j2) => {
+    j1.puntosMano = 0;
+    j2.puntosMano = 0;
 }
 
 //Retorna valor del 0 al 3. (4 posibles palos) 
@@ -165,11 +162,14 @@ const barajarRepartir = (jugador1, jugador2) => {
 }
 
 const compararValores = (jugador1, jugador2, cartaj1, cartaj2) => {
+    
     if(valores[traduccionPaloInverso(cartaj1.palo)][traduccionNumero(cartaj1.numero)] <=
         valores[traduccionPaloInverso(cartaj2.palo)][traduccionNumero(cartaj2.numero)] ) {
+            console.log("entro j2 suma")
             jugador2.puntosMano++;
             return 0;
         } else {
+            console.log("entro j1 suma")
             jugador1.puntosMano++;
             return 0;
         }
@@ -199,10 +199,8 @@ const vaciarCartas = () => {
 }
 
 const actualizarPuntuacion = (jugador1, jugador2) => {
-    const puntos = document.createElement("h3");
-    const destino = document.querySelector(".mesa-juego");
-    puntos.innerText=`${jugador1.puntosPartido} - ${jugador2.puntosPartido}`;
-    destino.appendChild(puntos);
+    const puntuacion = document.getElementById('puntos');
+    puntuacion.innerText=`${jugador1.nick}: ${jugador1.puntosPartido} - ${jugador2.nick}: ${jugador2.puntosPartido}`;
 } 
 
 const vaciarCartasJugadas = () => {
@@ -240,7 +238,7 @@ const jugadorGano = (jugador) => {
 }
 
 const imprimirCartas = (jugador1, jugador2) => {
-    //actualizarPuntuacion(jugador1, jugador2);
+    actualizarPuntuacion(jugador1, jugador2);
     jugador1.cartas.forEach(carta => {
         const card = document.createElement("div");
         card.classList.add("carta");
@@ -255,29 +253,22 @@ const imprimirCartas = (jugador1, jugador2) => {
         boton.addEventListener("click", () => {
             let palo = document.getElementById('paloj1');
             let numero = document.getElementById('numeroj1');
-            if(palo.innerText !== "" && numero.innerText !== "") {
-                /*const alerta = document.createElement("h3");
-                alerta.classList.add("alerta");
-                alerta.innerText = jugador1.nick + " ya jugó, compare cartas.";
-                const destinoAlerta = document.querySelector(".mesa-juego .cartas .jugadores .jugador1 .alerta");
-                destinoAlerta.appendChild(alerta);*/
-            } else {
+            if(palo.innerText == "" && numero.innerText == "") {
                 palo.innerText = carta.getPalo();
                 numero.innerText = carta.getNumero();
                 const CartaJugada = new Carta(palo.innerText, parseInt(numero.innerText));
                 eliminarCarta(jugador1, CartaJugada, vaciarCartas);
                 imprimirCartas(jugador1, jugador2);
-                let palo2 = document.getElementById('paloj2');
-                let numero2 = document.getElementById('numeroj2');
-                const cartaj2 = new Carta(palo2.innerText, parseInt(numero2.innerText));
                 if(!noJugoJ2()) {
+                    let palo2 = document.getElementById('paloj2');
+                    let numero2 = document.getElementById('numeroj2');
+                    const cartaj2 = new Carta(palo2.innerText, parseInt(numero2.innerText));
                     compararValores(jugador1, jugador2, CartaJugada, cartaj2);
                     vaciarCartasJugadas();
                 }
             }
         });
     });
-
     jugador2.cartas.forEach(carta => {
         const card = document.createElement("div");
         card.classList.add("carta");
@@ -292,18 +283,16 @@ const imprimirCartas = (jugador1, jugador2) => {
         boton.addEventListener("click", () => {
             let palo = document.getElementById('paloj2');
             let numero = document.getElementById('numeroj2');
-            if(palo.innerText !== "" && numero.innerText !== "") {
-                
-            } else {
+            if(palo.innerText == "" && numero.innerText == "") {
                 palo.innerText = carta.getPalo();
                 numero.innerText = carta.getNumero();
                 const CartaJugada = new Carta(palo.innerText, parseInt(numero.innerText));
                 eliminarCarta(jugador2, CartaJugada, vaciarCartas);
                 imprimirCartas(jugador1, jugador2);
-                let palo1 = document.getElementById('paloj1');
-                let numero1 = document.getElementById('numeroj1');
-                const cartaj1 = new Carta(palo1.innerText, parseInt(numero1.innerText));
                 if(!noJugoJ1()) {
+                    let palo1 = document.getElementById('paloj1');
+                    let numero1 = document.getElementById('numeroj1');
+                    const cartaj1 = new Carta(palo1.innerText, parseInt(numero1.innerText));
                     compararValores(jugador1, jugador2, cartaj1, CartaJugada);
                     vaciarCartasJugadas();
                 }
@@ -311,23 +300,39 @@ const imprimirCartas = (jugador1, jugador2) => {
         });
     });
     if(jugador1.cartas.length === 0 && jugador2.cartas.length === 0) {
+        if(!(jugador1.puntosMano + jugador2.puntosMano == 3)){
+            let palo1 = document.getElementById('paloj1');
+            let numero1 = document.getElementById('numeroj1');
+            const cartaj1 = new Carta(palo1.innerText, parseInt(numero1.innerText));
+            let palo2 = document.getElementById('paloj2');
+            let numero2 = document.getElementById('numeroj2');
+            const cartaj2 = new Carta(palo2.innerText, parseInt(numero2.innerText));
+            compararValores(jugador1, jugador2, cartaj1, cartaj2);
+        }
         if(!jugadorGano(jugador1) && !jugadorGano(jugador2)) {
-            if(jugador1.puntosMano == 2) {
-                jugador1.puntosPartido++
-                jugador1.puntosMano = 0;
-                jugador2.puntosMano = 0;
-            } else if(jugador2.puntosMano == 2) {
+            console.log(jugador1.puntosMano + " J1")
+            console.log(jugador2.puntosMano + " J2")
+
+            if(jugador1.puntosMano >= 2) {
+                jugador1.puntosPartido++;
+                actualizarPuntuacion(jugador1, jugador2);
+            } else if(jugador2.puntosMano >= 2) {
                 jugador2.puntosPartido++
-                jugador1.puntosMano = 0;
-                jugador2.puntosMano = 0;
-            } 
+                actualizarPuntuacion(jugador1, jugador2);
+            }
+            reiniciarPtosMano(jugador1, jugador2); 
             if(!jugadorGano(jugador1) && !jugadorGano(jugador2)) {
                 barajarRepartir(jugador1, jugador2);
                 imprimirCartas(jugador1, jugador2);
+            } else {
+                const marcador = document.getElementById('puntos');
+                if(jugadorGano(jugador1)) {
+                    marcador.innerText = `Ganó ${jugador1.nick}, fin del partido.`
+                } else {
+                    marcador.innerText = `Ganó ${jugador2.nick}, fin del partido.`
+                }
             }
-            } else if (jugadorGano(jugador1)) {
-            }   else {
-            } 
+        }
     }
 }
 
@@ -336,8 +341,8 @@ const jugarPartido = (jugador1, jugador2) => {
     jugador2.puntosPartido = 0;
     jugador1.puntosMano = 0;
     jugador2.puntosMano = 0;
-    barajarRepartir(jugador1, jugador2);
     actualizarPuntuacion(jugador1, jugador2);
+    barajarRepartir(jugador1, jugador2);
     imprimirCartas(jugador1, jugador2);
 }
 
